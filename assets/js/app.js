ddb82e9bf0524e53a9063f97d47ee6c6cfd20760
AsifKamalTias <asifkamaltias@gmail.com>
2023-10-15 15:27:52 +0600

    valiation done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,29 @@
 const app = angular.module('app', []);
 app.run(function ($rootScope) {
-    $rootScope.items = ['Bread', 'Egg'];
+    $rootScope.items = [];
+    $rootScope.error = false;
 });
 app.controller('addItemController', function ($scope, $rootScope) {
     $scope.item = '';
     $scope.addItem = function () {
+        if ($scope.item === '') {
+            $rootScope.error = true;
+            return;
+        }
         $rootScope.items.push($scope.item);
         $scope.item = '';
+        $rootScope.error = false;
     }
 });
 
 app.controller('itemController', function ($scope, $rootScope) {
     $scope.removeItem = function (index) {
         $rootScope.items.splice(index, 1);
+    }
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13) {
+        document.getElementById('add-btn').click();
     }
 });
